1d7f2f5227f4020ef745e2c1e6f86df0dd86d692
Arseniy Kucher <beeffallo@gmail.com>
2017-04-30 00:24:09 +0300

    Добавил FaceManager

---
+++ FaceRecognation/FaceRecognation.1.0/FaceManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Common;
+using Microsoft.ProjectOxford.Face.Contract;
+using Microsoft.ProjectOxford.Face;
+
+namespace FaceRecognation._1._0
+{
+	class FaceManager
+	{
+		public async Task<FaceRectangle[]> GetFaceRect()
+		{
+			return await UploadAndDetectFaces(filePath);
+		}
+	}
+}
