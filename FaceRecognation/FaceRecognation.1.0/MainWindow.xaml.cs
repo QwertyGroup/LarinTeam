0cc669c1f381c76d1c877e84c19cd98b06545047
Arseniy Kucher <beeffallo@gmail.com>
2017-04-30 00:18:21 +0300

    Добавил Json and MSProjectOxford

--- FaceRecognation/FaceRecognation.1.0/MainWindow.xaml.cs
+++ FaceRecognation/FaceRecognation.1.0/MainWindow.xaml.cs
@@ -12,12 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Microsoft.ProjectOxford.Common;
+using Microsoft.ProjectOxford.Face;
 
 namespace FaceRecognation._1._0
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
