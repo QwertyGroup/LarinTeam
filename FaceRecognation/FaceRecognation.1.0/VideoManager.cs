fc2d0a682c77214ce9ceb844fec8175f0b7abb56
harr0u <mrharrou@gmail.com>
2017-04-30 23:10:07 +0300

    VideoManager pererabotal nahuy, Batya v zdanii

--- FaceRecognation/FaceRecognation.1.0/VideoManager.cs
+++ FaceRecognation/FaceRecognation.1.0/VideoManager.cs
@@ -6,20 +6,38 @@ using System.Threading.Tasks;
 using Microsoft.ProjectOxford.Video;
 using Microsoft.ProjectOxford.Video.Contract;
 using System.IO;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace FaceRecognation._1._0
 {
-	public static class VideoManager
-	{
-		private static VideoServiceClient videoServiceClient = new VideoServiceClient(Synchron.getSecretCode());
+    public static class VideoManager
+    {
+        private static VideoServiceClient videoServiceClient = new VideoServiceClient(Synchron.getSecretCode());
 
-		private async void GetOperation(string filePath)
-		{
-			Operation videoOperation; // MARK KLASSNIY PAREN
-			using (var fs = new FileStream(@"C:\Videos\Sample.mp4", FileMode.Open))
-			{
-				videoOperation = await videoServiceClient.CreateOperationAsync(fs, OperationType.Stabilize);
-			}
-		}
-	}
+        public static async void getOperationAsync(string filePath)
+        {
+            Operation videoOperation;
+            using (var fs = new FileStream(filePath, FileMode.Open))
+            {
+                videoOperation = await videoServiceClient.CreateOperationAsync(fs, new FaceDetectionOperationSettings());
+            }
+
+            var operationResult = await videoServiceClient.GetOperationResultAsync(videoOperation);
+
+            while (true)
+            {
+                operationResult = await videoServiceClient.GetOperationResultAsync(videoOperation);
+                if (operationResult.Status == OperationStatus.Succeeded || operationResult.Status == OperationStatus.Failed)
+                {
+                    break;
+                }
+
+                Task.Delay(30000).Wait();
+            }
+            var motionDetectionJsonString = operationResult.ProcessingResult;
+            var motionDetection = JsonConvert.DeserializeObject<MotionDetectionResult>(motionDetectionJsonString);
+            Debug.WriteLine("END!");
+        }
+    }
 }
