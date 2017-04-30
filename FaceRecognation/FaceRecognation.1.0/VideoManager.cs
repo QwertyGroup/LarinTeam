6db2f5d53b84358a116e70dd5e4c5ce4fa51e026
Arseniy Kucher <beeffallo@gmail.com>
2017-04-30 23:02:34 +0300

    Test commit

--- FaceRecognation/FaceRecognation.1.0/VideoManager.cs
+++ FaceRecognation/FaceRecognation.1.0/VideoManager.cs
@@ -5,20 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.ProjectOxford.Video;
 using Microsoft.ProjectOxford.Video.Contract;
+using System.IO;
 
 namespace FaceRecognation._1._0
 {
-    public static class VideoManager
-    {
-        private static VideoServiceClient videoServiceClient = new VideoServiceClient(Synchron.getSecretCode());
+	public static class VideoManager
+	{
+		private static VideoServiceClient videoServiceClient = new VideoServiceClient(Synchron.getSecretCode());
 
-        private getOperation(string filePath)
-        {
-            Operation videoOperation;
-            using (var fs = new FileStream(@"C:\Videos\Sample.mp4", FileMode.Open))
-            {
-                videoOperation = await videoServiceClient.CreateOperationAsync(fs, OperationType.Stabilize);
-            }
-        }
-    }
+		private async void GetOperation(string filePath)
+		{
+			Operation videoOperation; // MARK KLASSNIY PAREN
+			using (var fs = new FileStream(@"C:\Videos\Sample.mp4", FileMode.Open))
+			{
+				videoOperation = await videoServiceClient.CreateOperationAsync(fs, OperationType.Stabilize);
+			}
+		}
+	}
 }
