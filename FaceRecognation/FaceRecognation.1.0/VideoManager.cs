57fcf5fd48d9cb342946d783bc3fd3de18c68a07
harr0u <mrharrou@gmail.com>
2017-04-30 23:03:41 +0300

    Added VideoManager

--- FaceRecognation/FaceRecognation.1.0/VideoManager.cs
+++ FaceRecognation/FaceRecognation.1.0/VideoManager.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.ProjectOxford.Video;
 using Microsoft.ProjectOxford.Video.Contract;
+using System.IO;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace FaceRecognation._1._0
 {
@@ -12,13 +15,29 @@ namespace FaceRecognation._1._0
     {
         private static VideoServiceClient videoServiceClient = new VideoServiceClient(Synchron.getSecretCode());
 
-        private getOperation(string filePath)
+        public static async void getOperationAsync(string filePath)
         {
             Operation videoOperation;
-            using (var fs = new FileStream(@"C:\Videos\Sample.mp4", FileMode.Open))
+            using (var fs = new FileStream(filePath, FileMode.Open))
             {
-                videoOperation = await videoServiceClient.CreateOperationAsync(fs, OperationType.Stabilize);
+                videoOperation = await videoServiceClient.CreateOperationAsync(fs, new FaceDetectionOperationSettings());
             }
+
+            var operationResult = await videoServiceClient.GetOperationResultAsync(videoOperation);
+
+            while (true)
+            {
+                operationResult = await videoServiceClient.GetOperationResultAsync(videoOperation);
+                if (operationResult.Status == OperationStatus.Succeeded || operationResult.Status == OperationStatus.Failed)
+                {
+                    break;
+                }
+
+                Task.Delay(30000).Wait();
+            }
+            var motionDetectionJsonString = operationResult.ProcessingResult;
+            var motionDetection = JsonConvert.DeserializeObject<MotionDetectionResult>(motionDetectionJsonString);
+            Debug.WriteLine("END!");
         }
     }
 }
