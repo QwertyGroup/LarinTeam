97101fe712ff017ff433f508bb6fec60c12455f4
harr0u <mrharrou@gmail.com>
2017-05-01 12:26:21 +0300

    Dobavil coordinati

--- FaceRecognation/FaceRecognation.1.0/VideoManager.cs
+++ FaceRecognation/FaceRecognation.1.0/VideoManager.cs
@@ -8,6 +8,7 @@ using Microsoft.ProjectOxford.Video.Contract;
 using System.IO;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace FaceRecognation._1._0
 {
@@ -15,7 +16,7 @@ namespace FaceRecognation._1._0
     {
         private static VideoServiceClient videoServiceClient = new VideoServiceClient(/*MSAPIManager.MSAPIManagerInstance.GetMSKey()*/"80c8c2aa437c4a30a76825d80efb2910 ");
 
-        public static async void getOperationAsync(string filePath)
+        public static async Task<FaceDetectionResult> getFaceDetectionAsync(string filePath)
         {
             Operation videoOperation;
             using (var fs = new FileStream(filePath, FileMode.Open))
@@ -35,7 +36,45 @@ namespace FaceRecognation._1._0
             }
             var faceDetectionTrackingResultJsonString = operationResult.ProcessingResult;
             var faceDetecionTracking = JsonConvert.DeserializeObject<FaceDetectionResult>(faceDetectionTrackingResultJsonString);
-            Debug.WriteLine("END!");
+            getFacesCoordinates(faceDetecionTracking);
+            return faceDetecionTracking;
+        }
+        public static void getFacesCoordinates(FaceDetectionResult faceDetectionTracking)
+        {
+            List<Image> resultFacec = new List<Image>();
+
+            List<int> IDs = faceDetectionTracking.FacesDetected.Select(x => x.FaceId).ToList();
+
+            var Fragments = faceDetectionTracking.Fragments.Where(x => x.Events != null).ToArray();
+
+            var idDict = getIdDict(Fragments);
+            Debug.WriteLine("TOPKEK");
+        }
+
+        private static Dictionary<int, Fragment<FaceEvent>> getIdDict(Fragment<FaceEvent>[] fragments)
+        {
+            Dictionary<int, List<Fragment<FaceEvent>>> IdDict = new Dictionary<int, List<Fragment<FaceEvent>>>();
+            foreach(var fragment in fragments)
+            {
+                for (int i = 0; i < fragment.Events.Length; i++)
+                {
+                    for (int j = 0; j < fragment.Events[i].Length; j++)
+                    {
+                        var curId = fragment.Events[i][j].Id;
+                        if (!IdDict.Keys.Contains(curId))
+                        {
+                            IdDict[curId] = new List<Fragment<FaceEvent>>();
+                        }
+                        IdDict[curId].Add(fragment);
+                    }
+                }
+            }
+            var coolIdDict = new Dictionary<int, Fragment<FaceEvent>>();
+            foreach (var key in IdDict.Keys)
+            {
+                coolIdDict[key] = IdDict[key][IdDict[key].Count / 2];
+            }
+            return coolIdDict;
         }
     }
 }
