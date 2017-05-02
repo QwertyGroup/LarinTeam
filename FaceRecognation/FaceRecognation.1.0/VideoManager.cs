f848536c1b07d3e3362b66fc9f40ae8fbae5709d
harr0u <mrharrou@gmail.com>
2017-05-02 18:30:21 +0300

    Made this govno kruche

--- FaceRecognation/FaceRecognation.1.0/VideoManager.cs
+++ FaceRecognation/FaceRecognation.1.0/VideoManager.cs
@@ -16,164 +16,182 @@ using MediaToolkit.Model;
 
 namespace FaceRecognation._1._0
 {
-	public static class VideoManager
-	{
-		private static VideoServiceClient videoServiceClient = new VideoServiceClient(KeyManager.Instance.MsVideoKey);
-		private static double TimeScale;
-		private static int VideoWidth;
-		private static int VideoHeight;
-		private static string CurrentVideoPath;
-
-		private static async Task<FaceDetectionResult> getFaceDetectionAsync(string filePath)
-		{
-			Operation videoOperation;
-			using (var fs = new FileStream(filePath, FileMode.Open))
-			{
-				videoOperation = await videoServiceClient.CreateOperationAsync(fs, new FaceDetectionOperationSettings());
-			}
-			OperationResult operationResult;
-			while (true)
-			{
-				operationResult = await videoServiceClient.GetOperationResultAsync(videoOperation);
-				if (operationResult.Status == OperationStatus.Succeeded || operationResult.Status == OperationStatus.Failed)
-				{
-					break;
-				}
-
-				Task.Delay(30000).Wait();
-			}
-			var faceDetectionTrackingResultJsonString = operationResult.ProcessingResult;
-			var faceDetecionTracking = JsonConvert.DeserializeObject<FaceDetectionResult>(faceDetectionTrackingResultJsonString);
-			TimeScale = faceDetecionTracking.Timescale;
-			return faceDetecionTracking;
-		}
-
-		private static Dictionary<int, CoolEvent> getCoolEvents(FaceDetectionResult faceDetectionTracking)
-		{
-			//List<int> IDs = faceDetectionTracking.FacesDetected.Select(x => x.FaceId).ToList();
-
-			var Fragments = faceDetectionTracking.Fragments.Where(x => x.Events != null).ToArray();
-
-			var idDict = getDictionary(Fragments);
-			return idDict;
-		}
-
-		private static List<Image> getFacesFromCoords(Dictionary<int, Fragment<FaceEvent>> faceCoordinates)
-		{
-			List<Image> CroppedFaces = new List<Image>();
+    public static class VideoManager
+    {
+        private static VideoServiceClient videoServiceClient = new VideoServiceClient(KeyManager.Instance.MsVideoKey);
+        private static double TimeScale;
+        private static int VideoWidth;
+        private static int VideoHeight;
+        private static string CurrentVideoPath;
 
 
-			return CroppedFaces;
-		}
+        private static async Task<FaceDetectionResult> getFaceDetectionAsync(string filePath)
+        {
+            Operation videoOperation;
+            using (var fs = new FileStream(filePath, FileMode.Open))
+            {
+                videoOperation = await videoServiceClient.CreateOperationAsync(fs, new FaceDetectionOperationSettings());
+            }
+            OperationResult operationResult;
+            while (true)
+            {
+                operationResult = await videoServiceClient.GetOperationResultAsync(videoOperation);
+                if (operationResult.Status == OperationStatus.Succeeded || operationResult.Status == OperationStatus.Failed)
+                {
+                    break;
+                }
 
-		private static void getFrame(string path, double startTime, int id)
-		{
-			if (!Directory.Exists("TempData"))
-				Directory.CreateDirectory("TempData");
+                Task.Delay(30000).Wait();
+            }
+            var faceDetectionTrackingResultJsonString = operationResult.ProcessingResult;
+            var faceDetecionTracking = JsonConvert.DeserializeObject<FaceDetectionResult>(faceDetectionTrackingResultJsonString);
+            TimeScale = faceDetecionTracking.Timescale;
+            return faceDetecionTracking;
+        }
 
-			var inputFile = new MediaFile() { Filename = path };
-			var outputFile = new MediaFile() { Filename = $@"TempData/{id}.png" };
+        private static Dictionary<int, List<CoolEvent>> getCoolEvents(FaceDetectionResult faceDetectionTracking)
+        {
+            //List<int> IDs = faceDetectionTracking.FacesDetected.Select(x => x.FaceId).ToList();
 
-			using (var engine = new Engine())
-			{
-				engine.GetMetadata(inputFile);
-				var options = new ConversionOptions() { Seek = TimeSpan.FromMilliseconds(startTime) };
-				engine.GetThumbnail(inputFile, outputFile, options);
-			}
-		}
+            var Fragments = faceDetectionTracking.Fragments.Where(x => x.Events != null).ToArray();
 
-		class CoolEvent
-		{
-			public FaceRectangle rec = new FaceRectangle();
-			public long startTime;
-			public int Id;
-		}
+            var idDict = getDictionary(Fragments);
 
-		private static Dictionary<int, CoolEvent> getDictionary(Fragment<FaceEvent>[] fragments)
-		{
-			Dictionary<int, List<CoolEvent>> dic = new Dictionary<int, List<CoolEvent>>();
+            return idDict;
+        }
 
-			foreach (var fragment in fragments)
-			{
-				var startTime = fragment.Start;
-				var interval = fragment.Interval;
+        private static void getFrame(string path, double startTime, int id)
+        {
+            if (!Directory.Exists("TempData"))
+                Directory.CreateDirectory("TempData");
 
-				for (int momentId = 0; momentId < fragment.Events.Length; momentId++)
-				{
-					long time = startTime + momentId * (long)interval;
-					foreach (var face in fragment.Events[momentId])
-					{
-						CoolEvent faceEvent = new CoolEvent
-						{
-							Id = face.Id,
-							startTime = time
-						};
+            var inputFile = new MediaFile() { Filename = path };
+            var outputFile = new MediaFile() { Filename = $@"TempData/{id}.{(long)startTime}.png" };
 
-						faceEvent.rec.Height = Convert.ToInt32(VideoHeight * face.Height);
-						faceEvent.rec.Width = Convert.ToInt32(VideoWidth * face.Width);
-						faceEvent.rec.Left = Convert.ToInt32(VideoWidth * face.X);
-						faceEvent.rec.Top = Convert.ToInt32(VideoHeight * face.Y);
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+                var options = new ConversionOptions() { Seek = TimeSpan.FromMilliseconds(startTime) };
+                engine.GetThumbnail(inputFile, outputFile, options);
+            }
+        }
 
-						if (faceEvent.rec.Height + faceEvent.rec.Top > VideoHeight)
-							faceEvent.rec.Height = VideoHeight - faceEvent.rec.Top;
-						if (faceEvent.rec.Width + faceEvent.rec.Left > VideoWidth)
-							faceEvent.rec.Width = VideoWidth - faceEvent.rec.Left;
+        class CoolEvent
+        {
+            public FaceRectangle rec = new FaceRectangle();
+            public long startTime;
+            public int Id;
+        }
 
-						if (!dic.Keys.Contains(faceEvent.Id))
-							dic[faceEvent.Id] = new List<CoolEvent>();
-						dic[faceEvent.Id].Add(faceEvent);
-					}
-				}
-			}
-			Dictionary<int, CoolEvent> coolDic = new Dictionary<int, CoolEvent>();
-			foreach (var key in dic.Keys)
-			{
-				coolDic[key] = dic[key][dic[key].Count / 2];
-			}
-			return coolDic;
-		}
+        private static Dictionary<int, List<CoolEvent>> getDictionary(Fragment<FaceEvent>[] fragments)
+        {
+            Dictionary<int, List<CoolEvent>> dic = new Dictionary<int, List<CoolEvent>>();
 
-		private static void setVideoResol()
-		{
-			MediaFile inputFile = new MediaFile() { Filename = CurrentVideoPath };
-			MediaFile testFile = new MediaFile() { Filename = "DeleteIt.png" };
-			Engine eng = new Engine();
-			eng.GetMetadata(inputFile);
+            foreach (var fragment in fragments)
+            {
+                var startTime = fragment.Start;
+                var interval = fragment.Interval;
 
-			var options = new ConversionOptions() { Seek = TimeSpan.FromSeconds(0) };
-			eng.GetThumbnail(inputFile, testFile, options);
-			Image testImage = ImageProcessing.ImageProcessingInstance.LoadImageFromFile("DeleteIt.png");
-			VideoWidth = testImage.Width;
-			VideoHeight = testImage.Height;
-		}
+                for (int momentId = 0; momentId < fragment.Events.Length; momentId++)
+                {
+                    long time = startTime + momentId * (long)interval;
+                    foreach (var face in fragment.Events[momentId])
+                    {
+                        CoolEvent faceEvent = new CoolEvent
+                        {
+                            Id = face.Id,
+                            startTime = time
+                        };
 
-		public static async void getFacesFromVideo(string path)
-		{
-			CurrentVideoPath = path;
-			setVideoResol();
+                        faceEvent.rec.Height = Convert.ToInt32(VideoHeight * face.Height);
+                        faceEvent.rec.Width = Convert.ToInt32(VideoWidth * face.Width);
+                        faceEvent.rec.Left = Convert.ToInt32(VideoWidth * face.X);
+                        faceEvent.rec.Top = Convert.ToInt32(VideoHeight * face.Y);
 
-			videoServiceClient.Timeout = TimeSpan.FromMinutes(15);
-			FaceDetectionResult faceDлулetectionResult = await getFaceDetectionAsync(path);
+                        if (faceEvent.rec.Height + faceEvent.rec.Top > VideoHeight)
+                            faceEvent.rec.Height = VideoHeight - faceEvent.rec.Top;
+                        if (faceEvent.rec.Width + faceEvent.rec.Left > VideoWidth)
+                            faceEvent.rec.Width = VideoWidth - faceEvent.rec.Left;
 
-			Debug.WriteLine("Got FDR!!!!)))");
+                        if (!dic.Keys.Contains(faceEvent.Id))
+                            dic[faceEvent.Id] = new List<CoolEvent>();
+                        dic[faceEvent.Id].Add(faceEvent);
+                    }
+                }
+            }
+            return dic;
+        }
 
-			// ONLY FOR TEST PURPS 
-			FaceDetectionResult faceDetectionResult = new FaceDetectionResult();
-			//PLS DELETE ME 
+        private static void setVideoResol()
+        {
+            MediaFile inputFile = new MediaFile() { Filename = CurrentVideoPath };
+            MediaFile testFile = new MediaFile() { Filename = "DeleteIt.png" };
+            Engine eng = new Engine();
+            eng.GetMetadata(inputFile);
 
-			Dictionary<int, CoolEvent> FaceIds = getCoolEvents(faceDetectionResult);
+            var options = new ConversionOptions() { Seek = TimeSpan.FromSeconds(0) };
+            eng.GetThumbnail(inputFile, testFile, options);
+            Image testImage = ImageProcessing.ImageProcessingInstance.LoadImageFromFile("DeleteIt.png");
+            VideoWidth = testImage.Width;
+            VideoHeight = testImage.Height;
+        }
 
-			foreach (int id in FaceIds.Keys)
-			{
-				var curEvent = FaceIds[id];
-				var startTimeMili = curEvent.startTime / TimeScale * 1000;
-				getFrame(path, startTimeMili, id);
+        public static async void getFacesFromVideo(string path)
+        {
+            CurrentVideoPath = path;
+            setVideoResol();
 
-				var img = ImageProcessing.ImageProcessingInstance.LoadImageFromFile($@"TempData/{id}.png");
-				img = ImageProcessing.ImageProcessingInstance.CropImage(img, FaceIds[id].rec);
-				ImageProcessing.ImageProcessingInstance.SaveImageToFile($@"TempData/{id}Face.png", img, System.Drawing.Imaging.ImageFormat.Png);
-				//File.Delete($@"TempData/{id}.png");
-			}
-		}
-	}
+            videoServiceClient.Timeout = TimeSpan.FromMinutes(15);
+            FaceDetectionResult faceDetectionResult = await getFaceDetectionAsync(path);
+
+            Debug.WriteLine("Got FDR!!!!)))");
+            Dictionary<int, List<CoolEvent>> FaceIds = getCoolEvents(faceDetectionResult);
+
+            //Choose 1 first and 4 random CoolEvent
+            FaceIds = chooseFive(FaceIds);
+
+            foreach (int id in FaceIds.Keys)
+            {
+                foreach (var curEvent in FaceIds[id])
+                {
+                    var startTimeMili = curEvent.startTime / TimeScale * 1000;
+                    getFrame(path, startTimeMili, id);
+
+                    var img = ImageProcessing.ImageProcessingInstance.LoadImageFromFile($@"TempData/{id}.{(long)startTimeMili}.png");
+                    img = ImageProcessing.ImageProcessingInstance.CropImage(img, curEvent.rec);
+                    ImageProcessing.ImageProcessingInstance.SaveImageToFile($@"TempData/{id}.{(long)startTimeMili}Face", img, System.Drawing.Imaging.ImageFormat.Png);
+                    //File.Delete($@"TempData/{id}.png");
+                }
+            }
+        }
+
+        private static Dictionary<int, List<CoolEvent>> chooseFive(Dictionary<int, List<CoolEvent>> faceIds)
+        {
+            var coolIds = new Dictionary<int, List<CoolEvent>>();
+            Random rnd = new Random();
+
+            foreach (var id in faceIds.Keys)
+            {
+                coolIds[id] = new List<CoolEvent>();
+                if (faceIds[id].Count > 4)
+                {
+                    //First
+                    coolIds[id].Add(faceIds[id].FirstOrDefault());
+
+                    //4 Random
+                    for (int i = 0; i < 4; i++)
+                    {
+                        var randomId = rnd.Next(1, faceIds[id].Count - 1);
+                        coolIds[id].Add(faceIds[id][randomId]);
+                    }
+                }
+                else
+                {
+                    coolIds[id].AddRange(faceIds[id]);
+                }
+            }
+
+            return coolIds;
+        }
+    }
 }
