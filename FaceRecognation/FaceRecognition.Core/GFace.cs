6b9532881a215951dd3dd02ca54b143714b5b11d
Arseniy Kucher <beeffallo@gmail.com>
2017-05-09 00:42:15 +0300

    GPerson & GFace

---
+++ FaceRecognation/FaceRecognition.Core/GFace.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceRecognition.Core
+{
+	class GFace // Generalizing not G )0))
+	{
+		public Image Img { get; set; }
+		public AddPersistedFaceResult FaceIdOnAdding { get; set; }
+	}
+
+	class GPerson
+	{
+		public string PersonGroupId { get; set; }
+		public CreatePersonResult PersonCreationId { get; set; }
+		public List<GFace> Faces { get; set; } = new List<GFace>();
+	}
+}
