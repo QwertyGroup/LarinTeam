6df93688a63244f8c15b8d1d1ab105645b7fc4f5
Arseniy Kucher <beeffallo@gmail.com>
2017-05-15 22:10:51 +0300

    Progress bar kek

---
+++ FaceRecognation/FaceRecognition.UI/RecognitionProgress.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FaceRecognition.UI
+{
+	public partial class RecognitionProgress : UserControl
+	{
+		public ProgressBar PBar { get { return pbProgressBar; } }
+		public double Progress { get { return pbProgressBar.Value; } set { pbProgressBar.Value = value; } }
+		public double IncreaseProgressBy { set { pbProgressBar.Value += value; } }
+		public string TStatus
+		{
+			get { return tbStatusBlack.Text; }
+			set
+			{
+				tbStatusBlack.Text = value;
+				tbStatusWhite.Text = value;
+			}
+		}
+		public RecognitionProgress()
+		{
+			InitializeComponent();
+		}
+	}
+}
