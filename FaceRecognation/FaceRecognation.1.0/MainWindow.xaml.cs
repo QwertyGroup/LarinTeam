5600e0666a7e060913d9ef8f761ae26539c1e8f7
Arseniy Kucher <beeffallo@gmail.com>
2017-05-29 22:59:14 +0300

    Some cctv work done ЫЫ)

--- FaceRecognation/FaceRecognation.1.0/MainWindow.xaml.cs
+++ FaceRecognation/FaceRecognation.1.0/MainWindow.xaml.cs
@@ -12,160 +12,160 @@ using System.Windows.Media.Imaging;
 
 namespace FaceRecognation._1._0
 {
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-			MessageManager.MsgManagerInstance.mainWindow = this;
-			Loaded += (s, e) => MessageManager.MsgManagerInstance.WriteMessage("Program started");
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MessageManager.MsgManagerInstance.mainWindow = this;
+            Loaded += (s, e) => MessageManager.MsgManagerInstance.WriteMessage("Program started");
 
-			FaceRecognition.Core.MessageManager.MsgManagerInstance.OnMessageSended +=
-				(s, e) => MessageManager.MsgManagerInstance.WriteMessage(e);
-			//Synchron.Instance.Test();
-		}
+            FaceRecognition.Core.MessageManager.MsgManagerInstance.OnMessageSended +=
+                (s, e) => MessageManager.MsgManagerInstance.WriteMessage(e);
+            //Synchron.Instance.Test();
+        }
 
-		private ImageProcessing _imgProcessing = ImageProcessing.ImageProcessingInstance;
-		private string _videoPath;
-		private Dictionary<int, List<System.Drawing.Image>> unchosenPhotos;
-		private int LoadedPeople = 0;
-		private List<System.Drawing.Image> Faces = new List<System.Drawing.Image>();
-		private void cmdTakePhoto_Click(object sender, RoutedEventArgs e)
-		{
-			var openDlg = new Microsoft.Win32.OpenFileDialog();
-			openDlg.Filter = "MP4 Video(*.mp4) | *.mp4"; //"JPEG Image(*.jpg)|*.jpg|PNG Image(*.png)|*.png|MP4 Video(*.mp4)|*.mp4";
-			bool? result = openDlg.ShowDialog(this);
+        private ImageProcessing _imgProcessing = ImageProcessing.ImageProcessingInstance;
+        private string _videoPath;
+        private Dictionary<int, List<System.Drawing.Image>> unchosenPhotos;
+        private int LoadedPeople = 0;
+        private List<System.Drawing.Image> Faces = new List<System.Drawing.Image>();
+        private void cmdTakePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            var openDlg = new Microsoft.Win32.OpenFileDialog();
+            openDlg.Filter = "MP4 Video(*.mp4) | *.mp4"; //"JPEG Image(*.jpg)|*.jpg|PNG Image(*.png)|*.png|MP4 Video(*.mp4)|*.mp4";
+            bool? result = openDlg.ShowDialog(this);
 
-			if (!(bool)result)
-			{
-				return;
-			}
+            if (!(bool)result)
+            {
+                return;
+            }
 
-			string filePath = openDlg.FileName;
-			cmdDetectFace.IsEnabled = true;
+            string filePath = openDlg.FileName;
+            cmdDetectFace.IsEnabled = true;
 
-			// video
-			_videoPath = filePath;
-			(sender as Button).Content = "Video selected";
-			MessageManager.MsgManagerInstance.WriteMessage("Video selected");
-			MessageManager.MsgManagerInstance.WriteMessage(_videoPath);
-		}
+            // video
+            _videoPath = filePath;
+            (sender as Button).Content = "Video selected";
+            MessageManager.MsgManagerInstance.WriteMessage("Video selected");
+            MessageManager.MsgManagerInstance.WriteMessage(_videoPath);
+        }
 
-		private System.Windows.Controls.Image GenerateImg(System.Drawing.Image photo)
-		{
-			Image img = new System.Windows.Controls.Image
-			{
-				Source = _imgProcessing.ConvertImageToBitmapImage(photo),
-				Height = 200,
-				Width = 200,
-				Stretch = Stretch.Uniform,
-				Margin = new Thickness(7),
-			};
-			img.MouseLeftButtonUp += (sender, e) =>
-			{
-				var tempImg = (Image)sender;
-				MessageManager.MsgManagerInstance.WriteMessage("Selected 1 face");
-				Faces.Add(ImageProcessing.ImageProcessingInstance.ConvertBitmapImageToImage((BitmapImage)img.Source));
-				if (LoadedPeople < unchosenPhotos.Keys.Count)
-				{
-					fillUndetectedFaces();
-					return;
-				}
-				if (LoadedPeople == unchosenPhotos.Keys.Count)
-				{
-					//fillUndetectedFaces();
-					DetectFacesPanel.Children.Clear();
-					notFaceButton.Visibility = Visibility.Collapsed;
-					cmdTakePhoto.Content = "Browse Video";
-					cmdDetectFace.Content = "Detect Faces";
-					cmdDetectFace.IsEnabled = false;
-					SavePhotos();
-				}
-			};
-			return img;
-		}
+        private System.Windows.Controls.Image GenerateImg(System.Drawing.Image photo)
+        {
+            Image img = new System.Windows.Controls.Image
+            {
+                Source = _imgProcessing.ConvertImageToBitmapImage(photo),
+                Height = 200,
+                Width = 200,
+                Stretch = Stretch.Uniform,
+                Margin = new Thickness(7),
+            };
+            img.MouseLeftButtonUp += (sender, e) =>
+            {
+                var tempImg = (Image)sender;
+                MessageManager.MsgManagerInstance.WriteMessage("Selected 1 face");
+                Faces.Add(ImageProcessing.ImageProcessingInstance.ConvertBitmapImageToImage((BitmapImage)img.Source));
+                if (LoadedPeople < unchosenPhotos.Keys.Count)
+                {
+                    fillUndetectedFaces();
+                    return;
+                }
+                if (LoadedPeople == unchosenPhotos.Keys.Count)
+                {
+                    //fillUndetectedFaces();
+                    DetectFacesPanel.Children.Clear();
+                    notFaceButton.Visibility = Visibility.Collapsed;
+                    cmdTakePhoto.Content = "Browse Video";
+                    cmdDetectFace.Content = "Detect Faces";
+                    cmdDetectFace.IsEnabled = false;
+                    SavePhotos();
+                }
+            };
+            return img;
+        }
 
-		private void SavePhotos()
-		{
-			if (!Directory.Exists("ResultFaces"))
-				Directory.CreateDirectory("ResultFaces");
-			foreach (var path in Directory.GetFiles("ResultFaces"))
-			{
-				File.Delete(path);
-			}
-			for (int i = 0; i < Faces.Count; i++)
-			{
-				ImageProcessing.ImageProcessingInstance.SaveImageToFile($"ResultFaces/{i}", Faces[i], System.Drawing.Imaging.ImageFormat.Png);
-				Faces[i].Dispose();
-			}
-			MessageManager.MsgManagerInstance.WriteMessage("Saved all faces to ResultFaces");
-		}
+        private void SavePhotos()
+        {
+            if (!Directory.Exists("ResultFaces"))
+                Directory.CreateDirectory("ResultFaces");
+            foreach (var path in Directory.GetFiles("ResultFaces"))
+            {
+                File.Delete(path);
+            }
+            for (int i = 0; i < Faces.Count; i++)
+            {
+                ImageProcessing.ImageProcessingInstance.SaveImageToFile($"ResultFaces/{i}", Faces[i], System.Drawing.Imaging.ImageFormat.Png);
+                Faces[i].Dispose();
+            }
+            MessageManager.MsgManagerInstance.WriteMessage("Saved all faces to ResultFaces");
+        }
 
-		private void fillUndetectedFaces()
-		{
-			notFaceButton.Visibility = Visibility.Visible;
-			DetectFacesPanel.Children.Clear();
-			var currentPerson = unchosenPhotos[unchosenPhotos.Keys.ElementAt(LoadedPeople)];
-			foreach (var person in currentPerson)
-			{
-				DetectFacesPanel.Children.Add(GenerateImg(person));
-			}
-			LoadedPeople++;
-		}
-		private async void cmdDetectFace_Click(object sender, RoutedEventArgs e)
-		{
-			LoadedPeople = 0;
-			Faces.Clear();
-			var btn = sender as Button;
-			btn.Content = "Detecting...";
-			MessageManager.MsgManagerInstance.WriteMessage(btn.Content.ToString());
+        private void fillUndetectedFaces()
+        {
+            notFaceButton.Visibility = Visibility.Visible;
+            DetectFacesPanel.Children.Clear();
+            var currentPerson = unchosenPhotos[unchosenPhotos.Keys.ElementAt(LoadedPeople)];
+            foreach (var person in currentPerson)
+            {
+                DetectFacesPanel.Children.Add(GenerateImg(person));
+            }
+            LoadedPeople++;
+        }
+        private async void cmdDetectFace_Click(object sender, RoutedEventArgs e)
+        {
+            LoadedPeople = 0;
+            Faces.Clear();
+            var btn = sender as Button;
+            btn.Content = "Detecting...";
+            MessageManager.MsgManagerInstance.WriteMessage(btn.Content.ToString());
 
-			// Detecting for Videos
-			unchosenPhotos = await VideoManager.VManagerInstance.GetFacesFromVideo(_videoPath);
+            // Detecting for Videos
+            unchosenPhotos = await VideoManager.VManagerInstance.GetFacesFromVideo(_videoPath);
 
-			btn.Content = "Detected successfuly.";
-			MessageManager.MsgManagerInstance.WriteMessage(btn.Content.ToString());
-			// Selecting One face from Five given
-			var faces = new List<System.Drawing.Image>();
-			if (unchosenPhotos.Count > 1)
-			{
-				MessageManager.MsgManagerInstance.WriteMessage($"Found {unchosenPhotos.Count} people");
-			}
-			else
-			{
-				MessageManager.MsgManagerInstance.WriteMessage($"Found {unchosenPhotos.Count} person");
-			}
-			fillUndetectedFaces();
-		}
+            btn.Content = "Detected successfuly.";
+            MessageManager.MsgManagerInstance.WriteMessage(btn.Content.ToString());
+            // Selecting One face from Five given
+            var faces = new List<System.Drawing.Image>();
+            if (unchosenPhotos.Count > 1)
+            {
+                MessageManager.MsgManagerInstance.WriteMessage($"Found {unchosenPhotos.Count} people");
+            }
+            else
+            {
+                MessageManager.MsgManagerInstance.WriteMessage($"Found {unchosenPhotos.Count} person");
+            }
+            fillUndetectedFaces();
+        }
 
-		private void cmdClearCache_Click(object sender, RoutedEventArgs e)
-		{
-			_imgProcessing.ClearCache();
-			if (Directory.Exists("TempData"))
-			{
-				Directory.Delete("TempData", true);
-			}
-			//spTakenPhotos.Children.Clear();
-			lbCompResults.Items.Clear();
-			MessageManager.MsgManagerInstance.WriteMessage("Cache cleared");
-		}
+        private void cmdClearCache_Click(object sender, RoutedEventArgs e)
+        {
+            _imgProcessing.ClearCache();
+            if (Directory.Exists("TempData"))
+            {
+                Directory.Delete("TempData", true);
+            }
+            //spTakenPhotos.Children.Clear();
+            lbCompResults.Items.Clear();
+            MessageManager.MsgManagerInstance.WriteMessage("Cache cleared");
+        }
 
-		private void notFaceButton_Click(object sender, RoutedEventArgs e)
-		{
-			if (LoadedPeople < unchosenPhotos.Keys.Count)
-			{
-				fillUndetectedFaces();
-				return;
-			}
-			if (LoadedPeople == unchosenPhotos.Keys.Count)
-			{
-				DetectFacesPanel.Children.Clear();
-				notFaceButton.Visibility = Visibility.Collapsed;
-				cmdTakePhoto.Content = "Browse Video";
-				cmdDetectFace.Content = "Detect Faces";
-				cmdDetectFace.IsEnabled = false;
-				SavePhotos();
-			}
-		}
-	}
+        private void notFaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoadedPeople < unchosenPhotos.Keys.Count)
+            {
+                fillUndetectedFaces();
+                return;
+            }
+            if (LoadedPeople == unchosenPhotos.Keys.Count)
+            {
+                DetectFacesPanel.Children.Clear();
+                notFaceButton.Visibility = Visibility.Collapsed;
+                cmdTakePhoto.Content = "Browse Video";
+                cmdDetectFace.Content = "Detect Faces";
+                cmdDetectFace.IsEnabled = false;
+                SavePhotos();
+            }
+        }
+    }
 }
--- FaceRecognation/TestCCTV/MainWindow.xaml.cs
+++ FaceRecognation/TestCCTV/MainWindow.xaml.cs
@@ -16,12 +16,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using static TestCCTV.UILinking;
+using System.Globalization;
 
 namespace TestCCTV
 {
     public partial class MainWindow : Window
     {
         private DispatcherTimer _dt = new DispatcherTimer();
+        private CameraSettings _currentCamera;
 
         public MainWindow()
         {
@@ -29,14 +32,14 @@ namespace TestCCTV
             Loaded += (s, e) =>
             {
                 _dt.Tick += (o, a) => Timer_Tick();
-                _dt.Interval = TimeSpan.FromMilliseconds(slDelay.Value);
+                SetDelay(SliderDelayConverter(5000));
+                _currentCamera = cbIp.SelectedValue as CameraSettings;
             };
         }
 
         private async void Timer_Tick()
         {
-            string url = "http://69.193.149.90:82/jpg/1/image.jpg";
-            var req = (HttpWebRequest)WebRequest.Create(url);
+            var req = (HttpWebRequest)WebRequest.Create(_currentCamera.GetImageUrl());
             var resp = await req.GetResponseAsync();
             var stream = resp.GetResponseStream();
             var img = System.Drawing.Image.FromStream(stream);
@@ -61,7 +64,120 @@ namespace TestCCTV
 
         private void SlDelay_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            _dt.Interval = TimeSpan.FromMilliseconds(e.NewValue);
+            if (IsLoaded)
+                SetDelay(SliderDelayConverter(e.NewValue));
         }
+
+        private void SetDelay((double Delay, string Tip) cr)
+        {
+            _dt.Interval = TimeSpan.FromMilliseconds(cr.Delay);
+            tbDelay.Text = cr.Tip;
+        }
+
+        private List<double> _delayStates = new List<double>
+        {
+            1d/60,
+            1d/30,
+            1d/20,
+            1d/15,
+            1d/10,
+            1d/5,
+            1,
+            2,
+            3,
+            5,
+            10,
+            30,
+            60,
+            120,
+            300,
+            600,
+            1800
+        };
+        private (double Delay, string Tip) SliderDelayConverter(double value)
+        {
+            var min = slDelay.Minimum;
+            var max = slDelay.Maximum - min;
+            value -= min;
+
+            var delay = 2d;
+            var tip = $"Default: {delay}s.";
+
+            var selctor = (int)Math.Floor(value / max * (_delayStates.Count - 1));
+            delay = _delayStates[selctor];
+            tip = (delay > 1) ? (delay > 60) ? $"{(int)(delay / 60)}min delay." :
+                $"{delay}s delay." : $"{1 / delay:0} fps.";
+
+            return (delay * 1000, tip);
+        }
+
+        private void CbIp_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _currentCamera = (sender as ComboBox).SelectedValue as CameraSettings;
+        }
+    }
+
+    internal class UILinking : DependencyObject
+    {
+        public class CameraSettings
+        {
+            public enum CameraManufacturer { Dlink, Axis, Bosh }
+
+            public string Ip { get; private set; }
+            public CameraManufacturer Manufacturer { get; set; }
+
+            private CameraSettings() { }
+            public CameraSettings(string ip, CameraManufacturer manufacturer)
+            {
+                Ip = ip;
+                Manufacturer = manufacturer;
+            }
+
+            public override string ToString()
+            {
+                return $"Ip: {Ip}; Manufacturer: {Manufacturer}.";
+            }
+
+            public string GetImageUrl()
+            {
+                var url = string.Empty;
+                switch (Manufacturer)
+                {
+                    case CameraManufacturer.Dlink:
+                        url = $"http://{Ip}/Image.jpg";
+                        break;
+                    case CameraManufacturer.Axis:
+                        url = $"http://{Ip}/jpg/1/image.jpg";
+                        break;
+                    case CameraManufacturer.Bosh:
+                        url = $"http://{Ip}/snap.jpg";
+                        break;
+                    default:
+                        break;
+                }
+                return url;
+            }
+        }
+
+        public UILinking()
+        {
+            Ips = new List<CameraSettings>
+           {
+               new CameraSettings ("81.149.56.38:8083", CameraSettings.CameraManufacturer.Axis),
+               new CameraSettings ("69.193.149.90:82", CameraSettings.CameraManufacturer.Axis),
+               new CameraSettings ("200.89.115.210", CameraSettings.CameraManufacturer.Dlink),
+               new CameraSettings ("89.113.5.135", CameraSettings.CameraManufacturer.Bosh),
+               new CameraSettings ("79.58.129.44:8083", CameraSettings.CameraManufacturer.Axis)
+           };
+        }
+
+        public List<CameraSettings> Ips
+        {
+            get { return (List<CameraSettings>)GetValue(IpsProperty); }
+            set { SetValue(IpsProperty, value); }
+        }
+
+        public static readonly DependencyProperty IpsProperty =
+            DependencyProperty.Register("Ips", typeof(List<CameraSettings>), typeof(UILinking), new PropertyMetadata(new List<CameraSettings>()));
     }
 }
